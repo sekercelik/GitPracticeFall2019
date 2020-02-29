dd1f93768f066489aa8e212ee158a82c67255b9f
sekercelik <sceliksidika@gmail.com>
2020-02-29 15:39:13 -0500

    added loop practice file

---
+++ src/day1/LoopPractice.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <20 ; i+=2) {
+            System.out.println(i);
+
+        }
+
+
+
+
+    }
+}
